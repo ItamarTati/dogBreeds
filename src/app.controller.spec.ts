636143c166d7f1b6903365099b75ab3299545c67
ItamarTati <itamar.tati1994@gmail.com>
2023-06-06 10:38:28 +0100

    added endpoint

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,22 +1,55 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AppService } from './app.service'
 
 describe('AppController', () => {
-  let appController: AppController;
+  let controller: AppController;
 
   beforeEach(async () => {
-    const app: TestingModule = await Test.createTestingModule({
-      controllers: [AppController],
+    const module: TestingModule = await Test.createTestingModule({
       providers: [AppService],
+      controllers: [AppController],
     }).compile();
 
-    appController = app.get<AppController>(AppController);
+    controller = module.get<AppController>(AppController);
   });
 
-  describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+  describe('getDogBreeds', () => {
+    it('should return an array of dog breeds', () => {
+      const result = controller.getDogBreeds();
+      expect(result).toBeDefined();
+      expect(result).toBeInstanceOf(Array);
+    });
+
+    it('should return dog breeds from the JSON file', () => {
+      const result = controller.getDogBreeds();
+      const expectedBreeds = [
+        {
+          name: "Labrador Retriever",
+          description: "Friendly, active, and outgoing"
+        },
+        {
+          name: "German Shepherd",
+          description: "Loyal, courageous, and intelligent"
+        },
+        {
+          name: "Golden Retriever",
+          description: "Intelligent, friendly, and devoted"
+        },
+        {
+          name: "Bulldog",
+          description: "Docile, willful, and friendly"
+        },
+        {
+          name: "Poodle",
+          description: "Intelligent, active, and alert"
+        },
+        {
+          name: "Beagle",
+          description: "Friendly, curious, and merry"
+        }
+      ]
+      expect(result).toEqual(expectedBreeds);
     });
   });
 });
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { readFileSync } from 'fs';
+import { join } from 'path'; 
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  private readonly dogData = JSON.parse(
+    readFileSync(join(__dirname, 'dog-breeds.json'), 'utf8')
+  );
+
+  getDogBreeds(): string {
+    return this.dogData.dogBreeds;
   }
 }
